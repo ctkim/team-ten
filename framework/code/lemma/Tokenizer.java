341dcbde18a6246ea308b346f65c1d516342211a
Ben <Ben>
2016-10-06 11:48:36 -0400

    Partitioned tokenizer

--- framework/code/lemma/Tokenizer.java
+++ framework/code/lemma/Tokenizer.java
@@ -7,9 +7,20 @@ public class Tokenizer {
 	public Tokenizer() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	//James
 	public List<String> tokenize(String sentence) {
 		// TODO implement your tokenizing code here
+		return null;
+	}
+
+	//Ben
+	private List<String> lemmatize(List<String> tokens) {
+		return null;
+	}
+
+	//Christine
+	private List<String> normalize(List<String> tokens) {
 		return null;
 	}
 }
