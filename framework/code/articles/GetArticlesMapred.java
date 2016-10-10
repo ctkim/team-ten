677c54447c188c084adeaaeab281d0b667f2143e
Christine Kim <Christine Kim>
2016-10-10 15:37:17 -0400

    update getarticles, include pom and sample wiki page

--- framework/code/articles/GetArticlesMapred.java
+++ framework/code/articles/GetArticlesMapred.java
@@ -1,14 +1,21 @@
-package code.articles;
+package getarticle;
 
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
+import util.WikipediaPageInputFormat;
 
 /**
  * This class is used for Section A of assignment 1. You are supposed to
@@ -24,30 +31,52 @@ public class GetArticlesMapred {
 	 * 		Page offset 	WikipediaPage
 	 * Output
 	 * 		Page offset 	WikipediaPage
-	 * @author Tuan
 	 *
 	 */
 	//@formatter:on
 	public static class GetArticlesMapper extends Mapper<LongWritable, WikipediaPage, Text, Text> {
 		public static Set<String> peopleArticlesTitles = new HashSet<String>();
+		// go through the titles and see if the people names match in the SETUP
+		// save the titles in the SET
+		// then in the mapper look for the titles and write it to output???
 
 		@Override
 		protected void setup(Mapper<LongWritable, WikipediaPage, Text, Text>.Context context)
 				throws IOException, InterruptedException {
-			// TODO: You should implement people articles load from
-			// DistributedCache here
+			// TODO: You should implement people articles load from DistributedCache here
 			super.setup(context);
 		}
 
 		@Override
 		public void map(LongWritable offset, WikipediaPage inputPage, Context context)
 				throws IOException, InterruptedException {
+			// i just wrote this to test 
+			// also tested if inputPage is null or empty; is NOT null or empty
+			String title = "Autonomous communities of Spain";
+			System.out.println(title); // prints
+			System.out.println(inputPage.getContent()); // does not print (string out of bounds exception)
 			// TODO: You should implement getting article mapper here
 		}
 	}
 
-	public static void main(String[] args) {
-		// TODO: you should implement the Job Configuration and Job call
-		// here
+	public static void main(String[] args) throws Exception{
+		Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+        	System.err.println("Usage: getarticlesmapred <in> <out>");
+        	System.exit(2);
+        }
+        
+		Job job = Job.getInstance(conf, "get articles");
+		// we have a custom input format
+		job.setInputFormatClass(WikipediaPageInputFormat.class);
+		
+        job.setJarByClass(GetArticlesMapred.class);
+        job.setMapperClass(GetArticlesMapper.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
